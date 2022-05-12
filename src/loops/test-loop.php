8f86d3c8a9802f5dd6637ac37594cd5cb5918c95
Piagrammist <Piagrammist@gmail.com>
2022-05-12 17:18:29 +0430

    Small fix

--- src/loops/test-loop.php
+++ src/loops/test-loop.php
@@ -1,7 +1,7 @@
 <?php
 
 return function () {
-    yield $this->sendMsg('me', '`Called the 'Test Loop' at '.\date('m/d H:i:s').'`', 'Markdown');
+    yield $this->sendMsg('me', "`Called the 'Test Loop' at ".\date('m/d H:i:s').'`', 'Markdown');
 
     return 20_000;  // Repeat every 20 seconds
 };
