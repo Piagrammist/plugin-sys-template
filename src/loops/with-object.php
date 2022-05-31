3a3bc2d4037be769e76658c4bccbc8ca46efcae1
Piagrammist <Piagrammist@gmail.com>
2022-05-31 06:32:59 +0430

    Small fix

--- src/loops/with-object.php
+++ src/loops/with-object.php
@@ -3,9 +3,7 @@
 use Piagrammist\PluginSys\GenericLoop;
 
 return function (GenericLoop $loop) {
-    yield $this->sendMsg('me', "`Called the 'Test Loop' at ".\date('m/d H:i:s').'`', 'Markdown');
-    yield $this->sendMsg('me', "`Stopping the loop...`", 'Markdown');
-    $loop->signal(true);  // Stop the loop
+    yield $this->sendMsg('me', "`Called the 'Test Loop (w/ object)' at ".\date('m/d H:i:s').'`', 'Markdown');
 
     return 20_000;  // Repeat every 20 seconds
 };
